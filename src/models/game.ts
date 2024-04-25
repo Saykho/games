41865d079424e54662b5316b9fafb1a11ee33425
ssar_say <sayhoo.saryglar@mail.ru>
2024-04-25 22:59:59 +0700

    добавила моковые данные

---
+++ src/models/game.ts
@@ -0,0 +1,13 @@
+export interface Game {
+    id: number;
+    title: string;
+    rating: number;
+    platform: string[];
+    multiplayer: {
+        offline: number;
+        online: number;
+    };
+    language: string[];
+    coverImage: string;
+    screenshots: string[];
+}
